354eca0c446f477964725d91a30a2dc6876a336c
Piriyatharshini <piriyatharshini@Piriyatharshini.local>
2016-09-29 22:36:53 +0530

    modified

--- MyFirstProject/js/script.js
+++ MyFirstProject/js/script.js
@@ -20,3 +20,71 @@ $(function () { //Same as document.addEventListener(DOMContentLoaded"...
 	});
 });
 
+//IIFE
+(function (global) {
+
+	var dc = {}; //namespace
+
+	//Convenience function for inserting innner HTML for 'select'
+	// ==> convenience method, so we dont have to write this from scratch every time because we are goint to have to do this several times. 
+	// ==> If you give me a selector and you give me the HTML, I will go ahead and take, select that selector.
+	// ==> Take that argument, grab that element I should say, the target element, and set its inner HTML to whatever the HTML string that you send me.
+	// ==> So this is just a convenience method to say, insertHTtml, pass it a selector, and the HTML string and it will insert it into the target element.
+	var insertHtml = function (selector, html) {
+		var targetElem = document.querySelector(selector);
+		targetElem.innerHTML = html;
+	};
+
+	//Show loading icon inside element identified by 'selector'.
+	/*var showLoading = function (selector) {
+		var html = "<div class='text-center'>";
+		html += "<img src='images/ajax-loader.gif'></div>";
+		insertHtml(selector, html);
+	};*/
+
+	//Return substitute of '{{propName}}'
+	//with propValue in given 'string'
+	var insertProperty = function (string, propName, propValue) {
+		var propToReplace = "{{" + propName + "}}";
+		string = string
+			.replace(new RegExp(propToReplace, "g"), propValue);
+		return string;
+	}
+
+
+	
+
+
+
+
+
+
+	global.$dc = dc; // Whatever we attach to the dc as a property is going to get exposed to the gloabal object in order for us to use it in some other page or some other script
+
+})(window);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
